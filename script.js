0816e29191a9b7fb7319c1677d1cbc269baee7f8
Marina Wolff <wolf0107@algonquinlive.com>
2022-02-24 11:30:35 -0500

    Fixes JavaScript - click eventListener

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-//Creates array and adds the images to the array
-const images = [ 'image-1', 'image-2', 'image-3', 'image-4', 'image-5', 'image-6', 'image-7', 'image-8', 'image-9', 'image-10', 'image-11', 'image-12'];
+//Creates array and adds objects with images and description to the array
+const images = [
+    {name: 'image-1', description: 'Image 1'},
+    {name: 'image-2', description: 'Image 2'},
+    {name: 'image-3', description: 'Image 3'},
+    {name: 'image-4', description: 'Image 4'},
+    {name: 'image-5', description: 'Image 5'},
+    {name: 'image-6', description: 'Image 6'},
+    {name: 'image-7', description: 'Image 7'},
+    {name: 'image-8', description: 'Image 8'},
+    {name: 'image-9', description: 'Image 9'},
+    {name: 'image-10', description: 'Image 10'},
+    {name: 'image-11', description: 'Image 11'},
+    {name: 'image-12', description: 'Image 12'},
+]
+
 
 //adds images dynamically to the gallery
 const $image = document.getElementById('img-gallery')
@@ -7,18 +21,19 @@ const $image = document.getElementById('img-gallery')
 const html = []
 
 for (const image of images){
-    html.push(`<div class="img" id="unique-image"><img src="images/${image}.jpg"></div>`)
+    html.push(`<div class="img" id="unique-image"><img data-description="${image.description}" data-image="${image.name}" src="images/${image.name}.jpg"></div>`)
 }
 
 $image.innerHTML = html.join('')
 
 //makes the images interactive
-//mouseover
+
+//mouseover eventListener
 const hover = document.querySelectorAll('.img')
 
 
 for(let i = 0; i < images.length; i++) {
-    hover[i].addEventListener("mouseover", function(){
+    hover[i].addEventListener("mouseover", function() {
         hover[i].style.opacity = 0.6;
 })};
 
@@ -28,7 +43,43 @@ hover[i].addEventListener("mouseout", function() {
 })};
 
 
-//click
+//click eventListener 
+display = 'name'
 
-const modal = document.getElementById(img-gallery)
+function switchDisplay(e){
+    const $allImages = document.querySelectorAll('.img')
 
+
+    console.log(e.target)
+
+    if(display === 'name'){
+            if(e.target.classList.contains('img')){
+            $image.innerHTML = `<p class = "text"> You clicked on ${e.target.dataset.description}!</p>`
+        }
+
+        display = 'description'
+    } else {
+           $image.innerHTML = html.join('')
+            
+           const hover = document.querySelectorAll('.img')
+
+
+        for(let i = 0; i < images.length; i++) {
+            hover[i].addEventListener("mouseover", function() {
+                hover[i].style.opacity = 0.6;
+        })};
+
+        for(let i = 0; i < images.length; i++) {
+        hover[i].addEventListener("mouseout", function() {
+            hover[i].style.opacity = 1.0;
+        })};
+
+    display = 'name'
+    }
+  }
+
+  $image.addEventListener('click', switchDisplay)
+
+
+
+//<img data-description="${image.description}" src="images/${e.target.dataset.name}.jpg"></div>`
